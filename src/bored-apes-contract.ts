20a7faa484c7a952e061901f3f618d09ca30c436
Simon Emanuel Schmid <simon@schmid.io>
2023-04-20 16:34:06 +0200

    Comment out failing code

--- src/bored-apes-contract.ts
+++ src/bored-apes-contract.ts
@@ -13,79 +13,79 @@ import { TokenMetadataTemplateInYaml as TokenMetadataTemplate } from "../generat
 const ipfsHash = "QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq";
 
 export function handleTransfer(event: TransferEvent): void {
-	let token = Token.load(event.params.tokenId.toString());
+  let token = Token.load(event.params.tokenId.toString());
 
-	if (!token) {
-		token = new Token(event.params.tokenId.toString());
-		token.tokenID = event.params.tokenId;
+  if (!token) {
+    token = new Token(event.params.tokenId.toString());
+    token.tokenID = event.params.tokenId;
 
-		token.tokenURI = "/" + event.params.tokenId.toString() + ".json";
-		const tokenIpfsHash = ipfsHash + token.tokenURI;
-		token.ipfsURI = tokenIpfsHash;
+    token.tokenURI = "/" + event.params.tokenId.toString();
+    const tokenIpfsHash = ipfsHash + token.tokenURI;
+    token.ipfsURI = tokenIpfsHash;
 
-		TokenMetadataTemplate.create(tokenIpfsHash);
-	}
-	token.owner = event.params.to;
-	token.updatedAtTimestamp = event.block.timestamp;
-	token.save();
+    TokenMetadataTemplate.create(tokenIpfsHash);
+  }
+  token.owner = event.params.to;
+  token.updatedAtTimestamp = event.block.timestamp;
+  token.save();
 }
 
 export function handleTokenMetadata(content: Bytes): void {
-	if (content) {
-		log.info("content: {}", [content.toString()]);
-	} else {
-		log.info("content: {}", ["null"]);
-	}
-	let tokenMetadata = new TokenMetadata(dataSource.stringParam());
-	const value = json.fromBytes(content).toObject();
+  if (content) {
+    log.info("content: {}", [content.toString()]);
+  } else {
+    log.info("content: {}", ["null"]);
+  }
+  let tokenMetadata = new TokenMetadata(dataSource.stringParam());
+  const value = json.fromBytes(content).toObject();
 
-	if (value) {
-		const image = value.get("image");
-		const attributesValue = value.get("attributes");
-		if (image) {
-			log.info("image: {}", [image.toString()]);
-		} else {
-			log.info("image: {}", ["null"]);
-		}
-		if (attributesValue) {
-			log.info("attributes: {}", [attributesValue.toString()]);
-		} else {
-			log.info("attributes: {}", ["null"]);
-		}
+  if (value) {
+    const image = value.get("image");
+    const attributesValue = value.get("attributes");
+    if (image) {
+      log.info("image: {}", [image.toString()]);
+    } else {
+      log.info("image: {}", ["null"]);
+    }
+    // if (attributesValue) {
+    //   log.info("attributes: {}", [attributesValue.toString()]);
+    // } else {
+    //   log.info("attributes: {}", ["null"]);
+    // }
 
-		if (image && attributesValue) {
-			tokenMetadata.image = image.toString();
-			log.info("image: {}", [tokenMetadata.image]);
-			log.info("attributes: {}", [attributesValue.toString()]);
+    if (image && attributesValue) {
+      tokenMetadata.image = image.toString();
+      log.info("image: {}", [tokenMetadata.image]);
+      //   log.info("attributes: {}", [attributesValue.toString()]);
 
-			const attributes = attributesValue.toArray();
-			if (attributes.length > 0) {
-				const attribute = attributes[0].toObject();
+      //   const attributes = attributesValue.toArray();
+      //   if (attributes.length > 0) {
+      //     const attribute = attributes[0].toObject();
 
-				// Access first attribute's properties using attribute.get('property_name')
-				const trait_type0 = attribute.get("trait_type");
-				const value0 = attribute.get("value");
+      //     // Access first attribute's properties using attribute.get('property_name')
+      //     const trait_type0 = attribute.get("trait_type");
+      //     const value0 = attribute.get("value");
 
-				if (trait_type0) {
-					log.info("trait_type0: {}", [trait_type0.toString()]);
-				} else {
-					log.info("trait_type0: {}", ["null"]);
-				}
-				if (value0) {
-					log.info("value0: {}", [value0.toString()]);
-				} else {
-					log.info("value0: {}", ["null"]);
-				}
+      //     if (trait_type0) {
+      //       log.info("trait_type0: {}", [trait_type0.toString()]);
+      //     } else {
+      //       log.info("trait_type0: {}", ["null"]);
+      //     }
+      //     if (value0) {
+      //       log.info("value0: {}", [value0.toString()]);
+      //     } else {
+      //       log.info("value0: {}", ["null"]);
+      //     }
 
-				// Add null checks before assigning values
-				if (trait_type0) {
-					tokenMetadata.trait_type0 = trait_type0.toString();
-				}
-				if (value0) {
-					tokenMetadata.value0 = value0.toString();
-				}
-			}
-		}
-		tokenMetadata.save();
-	}
+      //     // Add null checks before assigning values
+      //     if (trait_type0) {
+      //       tokenMetadata.trait_type0 = trait_type0.toString();
+      //     }
+      //     if (value0) {
+      //       tokenMetadata.value0 = value0.toString();
+      //     }
+      //   }
+    }
+    tokenMetadata.save();
+  }
 }
